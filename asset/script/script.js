399cded46ef1e9882f5e224da3257c6f0b93b914
mahdidhnv <140951036+mahdidhnv@users.noreply.github.com>
2024-01-25 11:30:09 -0800

    Add files via upload

---
+++ asset/script/script.js
@@ -0,0 +1,26 @@
+
+var myname='Mahdi Dehnavi'
+var job = 'Front-End Developer <br> Teacher'
+var greatind ='سلام <br> من مهدی دهنوی هستم ، معلم ابتدایی سابق وبرنامه نویس تازه کار فعلی ، این سایتی که میبینید اولین نمونه کار من هست ، امیدوارم که از این کار و نمونه کارهای دیگه هم خوشتون بیاد .'
+var tab1 ='من'
+var tab2='برنامه نویسی'
+var tab3='تدریس'
+var tab4='تماس با من'
+var ttab1 ='سلام دوباره هنوز مهدی دهنوی ام ، ساکن نیشابورو متولد فروردین 1378 در کرج ، سابقه کار های متفاوتی رو داشتم که تدریس و جدیدا برنامه نویسی جذاب ترینشون هست'
+var ttab2='به طور تخصصی دارم روی فرانت اند کار میکنم و هدف رسیدن به سطح فول استک هست ، اما وارد فیلد های دیگه هم تا حدی شدم ، برنامه نویسی رو دوست دارم چون میشه هر روز چیز جدیدی رو ازش یاد گرفت'
+var ttab3='دوران تدریس دوران طلایی زندگی من بود و اون دوران رو به دلیل پویایی فراوان و پاکی افرادی که هر روز نفس به نفس باهاشون بودم خیلیل دوست دارم  و قطعا در صورت امکان برمیگردم پای تخته'
+var ttab4=' telegram:@mahdidhnv <br>instagram:@mahdidhnv <br>linkedin:@mahdidhnv <br>phone:09397068594'
+
+
+
+document.getElementById('name').innerHTML=myname;
+document.getElementById('job').innerHTML=job;
+document.getElementById('greatind').innerHTML=greatind;
+document.getElementById('tab1').innerHTML=tab1;
+document.getElementById('tab2').innerHTML=tab2;
+document.getElementById('tab3').innerHTML=tab3;
+document.getElementById('tab4').innerHTML=tab4;
+document.getElementById('ttab1').innerHTML=ttab1;
+document.getElementById('ttab2').innerHTML=ttab2;
+document.getElementById('ttab3').innerHTML=ttab3;
+document.getElementById('ttab4').innerHTML=ttab4;
